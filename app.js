104086e49f3fb1677179ee509edbbf8774eeab83
martakunszt <kunsztowicz.m@gmail.com>
2019-11-11 10:01:15 +0000

    First commit backup

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Hello")
+
+let =document.getElementById == "slider"("style.css")
+document.getElementById('slider').onclick = function () {
+    if (document.getElementById('theme').href=="dark.css"){
+        document.getElementById('theme').href=='style.css';
+    }
+    else{
+        document.getElementById('theme').href == 'dark.css';
+    }
+};
+
+addEventListener("slider","dark.css")
