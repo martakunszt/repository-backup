4b1a557bc95ffa600f3967d3ccc5f7f65335490f
martakunszt <kunsztowicz.m@gmail.com>
2019-11-15 14:45:32 +0000

    Working on work.html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-alert("Hello")
+/*alert("Hello")
 
 let =document.getElementById == "slider"("style.css")
 document.getElementById('slider').onclick = function () {
@@ -10,4 +10,22 @@ document.getElementById('slider').onclick = function () {
     }
 };
 
-addEventListener("slider","dark.css")
+addEventListener("slider","dark.css")*/
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
